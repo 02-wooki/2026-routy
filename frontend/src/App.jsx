9a1fe389d994971b97636abc2d0fcca71bf674a7
wooki <hyunook02@naver.com>
2026-06-12 09:17:12 +0900

    feat(#3): React Router Dom 사용

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,49 +5,22 @@ import LoginPage from './pages/LoginPage'
 import CreateRoutePage from './pages/CreateRoutePage'
 import RouteDetailPage from './pages/RouteDetailPage'
 import Layout from './components/common/Layout'
-import { useAuthStore } from './store/useSocialStore'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
-  const { isAuthenticated } = useAuthStore()
-  const [currentPage, setCurrentPage] = useState('routes')
-  const [selectedRouteId, setSelectedRouteId] = useState(null)
-
-  // 로그인하지 않은 경우 로그인 페이지 표시
-  if (!isAuthenticated) {
-    return <LoginPage />
-  }
-
-  const handleViewRoute = (id) => {
-    setSelectedRouteId(id)
-    setCurrentPage('detail')
-  }
-
-  const renderContent = () => {
-    switch (currentPage) {
-      case 'routes':
-        return <FeedPage onAddRoute={() => setCurrentPage('create')} onViewRoute={handleViewRoute} />
-      case 'explore':
-        return <ExplorePage onViewRoute={handleViewRoute} />
-      case 'create':
-        return <CreateRoutePage onBack={() => setCurrentPage('routes')} />
-      case 'detail':
-        return selectedRouteId ? (
-          <RouteDetailPage routeId={selectedRouteId} onBack={() => setCurrentPage('routes')} />
-        ) : (
-          <FeedPage onAddRoute={() => setCurrentPage('create')} onViewRoute={handleViewRoute} />
-        )
-      default:
-        return <FeedPage onAddRoute={() => setCurrentPage('create')} onViewRoute={handleViewRoute} />
-    }
-  }
-
   return (
-    <Layout 
-      currentPage={currentPage === 'create' || currentPage === 'detail' ? 'routes' : currentPage} 
-      onPageChange={setCurrentPage}
-    >
-      {renderContent()}
-    </Layout>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+
+        <Route path="/" element={<Layout />}>
+          <Route index element={<FeedPage />} />
+          <Route path="/explore" element={<ExplorePage />} />
+          <Route path="/create" element={<CreateRoutePage />} />
+          <Route path="/detail/:id" element={<RouteDetailPage />} />
+        </Route>
+      </Routes>
+    </Router>
   )
 }
 
